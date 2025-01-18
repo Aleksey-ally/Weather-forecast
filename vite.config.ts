171b225a91e45338a009e93c85223095a69d71d4
Aleksey <easy.mail.to.copy@gmail.com>
2025-01-18 22:45:34 +0300

    fix: add config for vercel and vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+import {defineConfig} from 'vite'
+
+export default defineConfig(() => {
+
+    return {
+        plugins: [react()],
+        server: {
+            host: '0.0.0.0',
+            open: true,
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src')
+            }
+        },
+        define: {
+            __APP_ENV__: process.env.VITE_VERCEL_ENV,
+        },
     }
-  },
 })
